f3f171be565add7469e8da6167bc13c3a0ad77c6
UNIX <unix3dgforce@gmail.com>
2020-11-25 15:58:02 +0500

    reformat resources_test.py: asyncio.coroutine is Deprecated since version 3.8
    bugfix session.py:
    set custom header in parent class aiohttp.ClientSession in constructor of SessionWithHeaders class

--- yadisk_async/session.py
+++ yadisk_async/session.py
@@ -11,15 +11,17 @@ DEFAULT_USER_AGENT = "Python/%s.%s aiohttp/%s" % (sys.version_info.major,
                                                   sys.version_info.minor,
                                                   aiohttp.__version__)
 
+
 class SessionWithHeaders(aiohttp.ClientSession):
     """Just like your regular :any:`aiohttp.ClientSession` but with headers"""
 
     def __init__(self, *args, **kwargs):
         aiohttp.ClientSession.__init__(self, *args, **kwargs)
 
-        self.headers = CaseInsensitiveDict({
+        self.headers.update(CaseInsensitiveDict({
             "User-Agent": DEFAULT_USER_AGENT,
             "Accept-Encoding": ", ".join(("gzip", "deflate")),
             "Accept": "*/*",
             "Connection": "keep-alive"
-        })
+        }))
+
